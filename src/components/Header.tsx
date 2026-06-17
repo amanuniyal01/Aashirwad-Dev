8148d54f3c29086d3a80ae544baf3c3fc63fde7a
amanuniyal01 <amanuniyal315@gmail.com>
2026-06-17 19:57:45 +0530

    Created Header and successfully setup routes

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+function Header() {
+    const [isopen, setIsOpen] = useState(false)
+    return (
+        <div className="flex justify-between px-6 bg-black items-center ">
+
+            {/* Sidebar btn */}
+            <div className="flex gap-3 ">
+                <div className="text-white">
+                    =
+                </div>
+                <div>
+                    <img height={130} width={130} src="https://do6gp1uxl3luu.cloudfront.net/banner+and+logos/name.webp" />
+                </div>
+            </div>
+            <div >
+                <ul className="flex gap-8 items-center py-2 justify-center text-white">
+                    <li>Interview Practice</li>
+                    <li>Courses</li>
+                    <li>Explore</li>
+                    <li>Streak</li>
+                    <li>
+                        <img className="rounded-full h-10" src="https://lh3.googleusercontent.com/a/ACg8ocI3DRYAUlcRj7B4UBziOkpw-L_HzuSCOH1vS6agBl68Zh4MFvoUwQ=s96-c" alt="user picture" />
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
