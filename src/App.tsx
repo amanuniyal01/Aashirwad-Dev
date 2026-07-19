3885e969abca6dcc2988f38e6ae35ff968e93cb1
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-19 13:02:33 +0530

    Add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Route, Routes } from "react-router-dom"
 import { lazy, Suspense } from "react"
+import Loader from "./components/Loader/Loader";
 
 const Home = lazy(() => import("./components/Home/Home"));
 const Interview = lazy(() => import("./components/Interview/Interview"))
 const Layout = lazy(() => import('./components/Layout/Layout'))
-function App() {
+const Courses = lazy(() => import("./components/Courses/Courses"))
 
+function App() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="interview" element={<Interview />} />
-          <Route path="courses" element={<Interview />} />
+          <Route path="courses" element={<Courses />} />
         </Route>
       </Routes>
     </Suspense>
